9efc3639dc64dacb5be057a164631fbe8633c48f
David Dávila <david.davila@bitplaces.com>
2016-10-21 21:04:24 +0200

    Add empty view controllers

---
+++ TogglGoals/ProjectDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectDetailsViewController.swift
+//  TogglGoals
+//
+//  Created by David Davila on 21/10/2016.
+//  Copyright © 2016 davi. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectDetailsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ TogglGoals/ProjectsListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectsListViewController.swift
+//  TogglGoals
+//
+//  Created by David Davila on 21/10/2016.
+//  Copyright © 2016 davi. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectsListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
