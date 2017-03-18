a03e21195a7864dc5812448bbc5b4dea891d7ae9
David Dávila <david.davila@bitplaces.com>
2017-03-18 13:00:28 +0100

    Fix project ID key in RunningEntry.fromTogglAPI()

--- TogglGoals/Operations/NetworkRetrieveRunningEntryOperation.swift
+++ TogglGoals/Operations/NetworkRetrieveRunningEntryOperation.swift
@@ -30,7 +30,7 @@ extension RunningEntry {
     static func fromTogglAPI(dictionary: StringKeyedDictionary) -> RunningEntry? {
         let dateFormatter = ISO8601DateFormatter()
         if let id = dictionary["id"] as? Int64,
-            let projectId = dictionary["projectId"] as? Int64,
+            let projectId = dictionary["pid"] as? Int64,
             let start = dictionary["start"] as? String,
             let at = dictionary["at"] as? String,
             let parsedStart = dateFormatter.date(from: start),
