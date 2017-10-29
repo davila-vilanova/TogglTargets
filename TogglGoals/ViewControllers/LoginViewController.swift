b5d877c754dceb309aea5a626ed63a8b6f9b5bb0
David Dávila <david@davi.la>
2017-10-29 21:34:30 +0100

    Add not-yet-wired login button

--- TogglGoals/ViewControllers/LoginViewController.swift
+++ TogglGoals/ViewControllers/LoginViewController.swift
@@ -82,6 +82,7 @@ class LoginViewController: NSViewController, ViewControllerContaining {
     @IBOutlet weak var loginMethodUsernameItem: NSMenuItem!
     @IBOutlet weak var loginMethodAPITokenItem: NSMenuItem!
     @IBOutlet weak var credentialsView: NSView!
+    @IBOutlet weak var loginButton: NSButton!
     @IBOutlet weak var progressIndicator: NSProgressIndicator!
     @IBOutlet weak var resultLabel: NSTextField!
     @IBOutlet weak var profileImageView: NSImageView!
