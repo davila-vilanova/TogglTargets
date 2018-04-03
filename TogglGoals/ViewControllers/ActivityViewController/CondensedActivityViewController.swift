f59fbc44fd5091af246f8af4a3ae156210eecccb
David Dávila <david@davi.la>
2018-04-02 20:02:51 -0500

    Show short error description in condensed view.
    Show a short error description in the condensed activity view if there a single error occurred. Keep showing count of errors if there are multiple ones.

--- TogglGoals/ViewControllers/ActivityViewController/CondensedActivityViewController.swift
+++ TogglGoals/ViewControllers/ActivityViewController/CondensedActivityViewController.swift
@@ -45,11 +45,11 @@ class CondensedActivityViewController: NSViewController {
         case .syncing(let count):
             self.statusDescriptionLabel.stringValue = "Syncing..."
             self.setStatusDetail("\(count) syncing \(count == 1 ? "activity" : "activities") in progress")
-        case .errors(let count):
-            self.statusDescriptionLabel.stringValue = (count == 1 ? "Error" : "Errors") + " syncing data"
-            self.setStatusDetail((count == 1 ? "An error" : "\(count) errors") + " occurred.")
+        case .errors(let count, let firstError):
+            self.statusDescriptionLabel.stringValue = "Could Not Syncronize All Data"
+            self.setStatusDetail((count == 1 ? firstError.shortDescriptionForUser : "\(count) syncing operations have errors"))
         case .success(let count):
-            self.statusDescriptionLabel.stringValue = "All data synchronized"
+            self.statusDescriptionLabel.stringValue = "All Data Synchronized"
             self.setStatusDetail(nil)
         case .idle:
             break
@@ -83,7 +83,7 @@ class CondensedActivityViewController: NSViewController {
 
 fileprivate enum State {
     case syncing(count: Int)
-    case errors(count: Int)
+    case errors(count: Int, first: APIAccessError)
     case success(count: Int)
     case idle
 }
@@ -91,13 +91,14 @@ fileprivate enum State {
 fileprivate func stateProducer(from statusesProducer: SignalProducer<[ActivityStatus], NoError>)
     -> SignalProducer<State, NoError> {
         let syncingCount = statusesProducer.map { $0.filter { $0.isExecuting } }.map { $0.count }
-        let errorCount = statusesProducer.map { $0.filter { $0.isError } }.map { $0.count }
+        let errorCountAndFirst = statusesProducer.map { $0.filter { $0.isError } }.map { ($0.count, $0.first?.error) }
         let successCount = statusesProducer.map { $0.filter { $0.isSuccessful } }.map { $0.count }
 
-        return SignalProducer.combineLatest(syncingCount, errorCount, successCount)
-            .map { (syncingCount, errorCount, successCount) in
+        return SignalProducer.combineLatest(syncingCount, successCount, errorCountAndFirst)
+            .map { (syncingCount, successCount, errorCountAndFirst) in
+                let (errorCount, firstError) = errorCountAndFirst
                 if errorCount > 0 {
-                    return .errors(count: errorCount)
+                    return .errors(count: errorCount, first: firstError!)
                 } else if syncingCount > 0 {
                     return .syncing(count: syncingCount)
                 } else if successCount > 0 {
@@ -106,4 +107,25 @@ fileprivate func stateProducer(from statusesProducer: SignalProducer<[ActivitySt
                     return .idle
                 }
         }
+}
+
+fileprivate extension APIAccessError {
+    var shortDescriptionForUser: String {
+        switch self {
+        case .noCredentials:
+            return "No credentials Configured"
+        case .authenticationError(response: _):
+            return "Authentication Error"
+        case .loadingSubsystemError(underlyingError: let underlyingError):
+            return underlyingError.localizedDescription
+        case .serverHiccups(response: let response, data: _):
+            return "Server Error (\(response.statusCode))"
+        case .invalidJSON(underlyingError: _, data: _):
+            return "Unexpected JSON in Response"
+        case .nonHTTPResponseReceived(response: _):
+            return "Unexpected Response Type"
+        case .otherHTTPError(response: let response):
+            return "HTTP error (\(response.statusCode))"
+        }
+    }
 }
