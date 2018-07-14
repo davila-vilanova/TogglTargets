7793f5021ef2dce64c302fcd0c108710deeed6c6
David Dávila <david@davi.la>
2018-07-15 00:35:52 +0200

    Internationalize GoalStrategyViewController

--- TogglGoals/ViewControllers/GoalStrategyViewController.swift
+++ TogglGoals/ViewControllers/GoalStrategyViewController.swift
@@ -63,9 +63,19 @@ class GoalStrategyViewController: NSViewController, BindingTargetProvider {
 
         // Update total hours and hours per day with the values of the corresponding signals
         totalHoursStrategyField.reactive.text <~ timeGoal.mapToString(timeFormatter: timeFormatter)
-            .map { "to reach your goal of \($0)" }
+            .map {
+                String.localizedStringWithFormat(
+                    NSLocalizedString("goal-strategy.header",
+                                      comment: "header of the strategy section -- describing the target hours of the goal"),
+                    $0)
+        }
         baselineField.reactive.text <~ dayBaselineAdjustedToProgress.mapToString(timeFormatter: timeFormatter)
-            .map { "work \($0) per day" }
+            .map {
+                String.localizedStringWithFormat(
+                    NSLocalizedString("goal-strategy.adjusted-baseline",
+                                      comment: "amount of time to work per day to achieve the goal (adjusted day baseline)"),
+                    $0)
+        }
 
         let formattedDifferential = dayBaselineDifferential
             .map { $0?.magnitude }
@@ -79,7 +89,8 @@ class GoalStrategyViewController: NSViewController, BindingTargetProvider {
                                                                      dayBaseline.filter { $0 == nil },
                                                                      dayBaselineDifferential.filter { $0 == nil },
                                                                      feasibility.filter { $0 == nil })
-            .map { _ in "The day baseline could not be calculated" }
+            .map { _ in NSLocalizedString("goal-strategy.cannot-calculate-baseline",
+                                          comment: "the adjusted day baseline could not be calculated") }
 
         let feasibleCaseDescriptions =
             SignalProducer.combineLatest(feasibility.skipNil(),
@@ -91,18 +102,34 @@ class GoalStrategyViewController: NSViewController, BindingTargetProvider {
                     return feasibility.isFeasible
                 }.map { (_, differential, formattedDifferential, baseline, formattedBaseline) -> String in
                     if abs(differential) < 0.01 {
-                        return "That pretty much matches your baseline of \(formattedBaseline)"
+                        return String.localizedStringWithFormat(
+                            NSLocalizedString("goal-strategy.adusted-baseline-matches",
+                                              comment: "the adjusted baseline approximately matches the a priori baseline"),
+                            formattedBaseline)
+                    } else if differential > 0 {
+                        return String.localizedStringWithFormat(
+                            NSLocalizedString("goal-strategy.differential.more",
+                                              comment: "need to catch up to achieve the goal"),
+                            formattedDifferential, formattedBaseline)
                     } else {
-                        let adverb = differential > 0 ? "more" : "less"
-                        return "That is \(formattedDifferential) \(adverb) than your baseline of \(formattedBaseline)"
+                        return String.localizedStringWithFormat(
+                            NSLocalizedString("goal-strategy.differential.less",
+                                              comment: "could work less and still achieve the goal"),
+                            formattedDifferential, formattedBaseline)
                     }
         }
 
         let unfeasibleCaseDescriptions = feasibility.skipNil().filter { $0.isUnfeasible }
-            .map { _ in "This may be possible if you sleep very little and have superhuman focusing abilities"  }
+            .map { _ in
+                NSLocalizedString("goal-strategy.unfeasible",
+                                          comment: "reaching the goal is unfeasible but not impossible")
+        }
 
         let impossibleCaseDescriptions = feasibility.skipNil().filter { $0.isImpossible }
-            .map { _ in "This would require more than a full day of work per day" }
+            .map { _ in
+                NSLocalizedString("goal-strategy.impossible",
+                                  comment: "reaching the goal is impossible")
+        }
 
         baselineDifferentialField.reactive.stringValue <~
             SignalProducer.merge(feasibleCaseDescriptions, unfeasibleCaseDescriptions, impossibleCaseDescriptions, baselineCalculationErrors)
@@ -136,6 +163,10 @@ class GoalReachedViewController: NSViewController, BindingTargetProvider {
 
         let timeGoal = lastBinding.latestOutput { $0 }
         goalReachedField.reactive.text <~ timeGoal.mapToString(timeFormatter: timeFormatter)
-            .map { "you have reached your goal of \($0)" }
+            .map {
+                String.localizedStringWithFormat(
+                    NSLocalizedString("goal-reached", comment: "the goal has been achieved"),
+                    $0)
+        }
     }
 }
