7a3f37ae4bdd47fccd418945ad0b462a893b23dc
David Dávila <david@davi.la>
2018-08-16 12:58:50 +0200

    Remove unused (bg-)color-view

--- TogglGoals/ViewControllers/ProjectDetailsViewController.swift
+++ TogglGoals/ViewControllers/ProjectDetailsViewController.swift
@@ -144,22 +144,3 @@ class ProjectDetailsViewController: NSViewController, BindingTargetProvider {
         }
     }
 }
-
-class ColoredView: NSView {
-    var backgroundColor: NSColor? {
-        didSet {
-            let cgColor: CGColor?
-            if let color = backgroundColor {
-                cgColor = color.cgColor
-            } else {
-                cgColor = nil
-            }
-
-            // TODO: learn about the right timing to apply the color without this megahack
-            let delay = DispatchTime.now() + .seconds(1)
-            DispatchQueue.main.asyncAfter(deadline: delay) {
-                self.layer?.backgroundColor = cgColor
-            }
-        }
-    }
-}
