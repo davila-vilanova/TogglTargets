5a34c0d3f2c2534df2a4011c7c47cef2722cf87e
David Dávila <david@davi.la>
2018-10-17 22:32:56 +0200

    Keep onboarding-related strings in onboarding folder

--- TogglGoals/Onboarding/OnboardingSteps.swift
+++ TogglGoals/Onboarding/OnboardingSteps.swift
@@ -21,17 +21,19 @@ enum OnboardingStepIdentifier: String {
     case seeDayProgress
 }
 
+fileprivate let StringsTableName = "OnboardingSteps"
+
 fileprivate let OnboardingSteps: [OnboardingStep] = [
-    OnboardingStep(identifier: .login, text: NSLocalizedString("onboarding.step.login", comment: "onboarding step: login")),
-    OnboardingStep(identifier: .closeLogin, text: NSLocalizedString("onboarding.step.close-login", comment: "onboarding step: close login")),
-    OnboardingStep(identifier: .selectProject, text: NSLocalizedString("onboarding.step.select-project", comment: "onboarding step: select project")),
-    OnboardingStep(identifier: .createGoal, text: NSLocalizedString("onboarding.step.create-goal", comment: "onboarding step: create goal")),
-    OnboardingStep(identifier: .setTargetHours, text: NSLocalizedString("onboarding.step.set-target-hours", comment: "onboarding step: set target hours"), allowContinue: true, preferredEdge: .maxY),
-    OnboardingStep(identifier: .setWorkWeekdays, text: NSLocalizedString("onboarding.step.set-work-weekdays", comment: "onboarding step: set work weekdays"), allowContinue: true),
-    OnboardingStep(identifier: .selectComputeStrategyFrom, text: NSLocalizedString("onboarding.step.select-compute-from", comment: "onboarding step: select from which day to compute the goal strategy"), allowContinue: true),
-    OnboardingStep(identifier: .seeTimeProgress, text: NSLocalizedString("onboarding.step.see-time-progress", comment: "onboarding step: see time progress"), allowContinue: true),
-    OnboardingStep(identifier: .seeGoalStrategy, text: NSLocalizedString("onboarding.step.see-goal-strategy", comment: "onboarding step: see strategy to fulfill goal"), allowContinue: true),
-    OnboardingStep(identifier: .seeDayProgress, text: NSLocalizedString("onboarding.step.see-day-progress", comment: "onboarding step: see day progress"), allowContinue: true),
+    OnboardingStep(identifier: .login, text: NSLocalizedString("onboarding.step.login", tableName: StringsTableName, comment: "onboarding step: login")),
+    OnboardingStep(identifier: .closeLogin, text: NSLocalizedString("onboarding.step.close-login", tableName: StringsTableName, comment: "onboarding step: close login")),
+    OnboardingStep(identifier: .selectProject, text: NSLocalizedString("onboarding.step.select-project", tableName: StringsTableName, comment: "onboarding step: select project")),
+    OnboardingStep(identifier: .createGoal, text: NSLocalizedString("onboarding.step.create-goal", tableName: StringsTableName, comment: "onboarding step: create goal")),
+    OnboardingStep(identifier: .setTargetHours, text: NSLocalizedString("onboarding.step.set-target-hours", tableName: StringsTableName, comment: "onboarding step: set target hours"), allowContinue: true, preferredEdge: .maxY),
+    OnboardingStep(identifier: .setWorkWeekdays, text: NSLocalizedString("onboarding.step.set-work-weekdays", tableName: StringsTableName, comment: "onboarding step: set work weekdays"), allowContinue: true),
+    OnboardingStep(identifier: .selectComputeStrategyFrom, text: NSLocalizedString("onboarding.step.select-compute-from",  tableName: StringsTableName, comment: "onboarding step: select from which day to compute the goal strategy"), allowContinue: true),
+    OnboardingStep(identifier: .seeTimeProgress, text: NSLocalizedString("onboarding.step.see-time-progress", tableName: StringsTableName, comment: "onboarding step: see time progress"), allowContinue: true),
+    OnboardingStep(identifier: .seeGoalStrategy, text: NSLocalizedString("onboarding.step.see-goal-strategy", tableName: StringsTableName, comment: "onboarding step: see strategy to fulfill goal"), allowContinue: true),
+    OnboardingStep(identifier: .seeDayProgress, text: NSLocalizedString("onboarding.step.see-day-progress", tableName: StringsTableName, comment: "onboarding step: see day progress"), allowContinue: true),
 ]
 
 func onboardingSteps(startingFrom initialStepIdentifier: OnboardingStepIdentifier? = nil) -> [OnboardingStep] {
