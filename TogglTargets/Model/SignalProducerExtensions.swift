2c87ee8709ec164e89f38b789a6beedef6c3e856
David Dávila <david@davi.la>
2018-11-13 14:58:15 -0500

    doc

--- TogglTargets/Model/SignalProducerExtensions.swift
+++ TogglTargets/Model/SignalProducerExtensions.swift
@@ -12,8 +12,17 @@ import Result
 
 private let defaultPlaceholderForNil = "--"
 
-// TODO: Extension on DateComponentsFormatter?
 extension SignalProducer where Value == TimeInterval? {
+    /// Transforms a producer of optional `TimeInterval` values into a producer of string values using the provided
+    /// `DateComponentsFormatter`.
+    ///
+    /// - parameters:
+    ///   - timeFormatter: The `TimeFormatter` to format each of the `TimeInterval` values emitted by this producer
+    ///                    into a string.
+    ///   - placeholderForNil: The value to emit when this producer emits a `nil` value or if the formatter
+    ///                        returs `nil`.
+    ///
+    /// - returns: A producer of formatted strings, each representing a time a interval value or a `nil` value.
     func mapToString(timeFormatter: DateComponentsFormatter,
                      placeholderForNil: String = defaultPlaceholderForNil) -> SignalProducer<String, Error> {
         return map { (timeOrNil: TimeInterval?) -> String in
@@ -26,6 +35,15 @@ extension SignalProducer where Value == TimeInterval? {
 }
 
 extension SignalProducer where Value == TimeInterval {
+    /// Transforms a producer of `TimeInterval` values into a producer of string values using the provided
+    /// `DateComponentsFormatter`.
+    ///
+    /// - parameters:
+    ///   - timeFormatter: The `TimeFormatter` to format each of the non `nil` values emitted by this producer into a
+    ///                    string.
+    ///   - placeholderForNil: The value to emit if the formatter returs nil.
+    ///
+    /// - returns: A producer of formatted strings, each representing a time a interval value or a `nil` value.
     func mapToString(timeFormatter: DateComponentsFormatter,
                      placeholderForNil: String = defaultPlaceholderForNil) -> SignalProducer<String, Error> {
         return map { (time: TimeInterval) -> String in
@@ -34,8 +52,16 @@ extension SignalProducer where Value == TimeInterval {
     }
 }
 
-// TODO: Extension in NumberFormatter?
 extension SignalProducer where Value == NSNumber? {
+    /// Transforms a producer of optional `NSNumber` values into a producer of string values using the provided
+    /// `NumberFormatter`.
+    ///
+    /// - parameters:
+    ///   - timeFormatter: The `NumberFormatter` to format each of the non `nil` values emitted by this producer into a
+    ///                    string.
+    ///   - placeholderForNil: The value to emit when this producer emits a `nil` value or if the formatter returs nil.
+    ///
+    /// - returns: A producer of formatted strings, each representing a formatted numeric value or a `nil` value.
     func mapToNumberFormattedString(numberFormatter: NumberFormatter,
                                     placeholderForNil: String = defaultPlaceholderForNil)
         -> SignalProducer<String, Error> {
