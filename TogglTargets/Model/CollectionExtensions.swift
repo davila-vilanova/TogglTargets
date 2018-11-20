cfaf4db119aaf81b3e7d965a9dfaba7a51b9e7ad
David Dávila <david@davi.la>
2018-11-20 23:49:22 +0100

    Correct misplaced comment

--- TogglTargets/Model/CollectionExtensions.swift
+++ TogglTargets/Model/CollectionExtensions.swift
@@ -8,14 +8,15 @@
 
 import Foundation
 
-/// Copies this dictionary substituing or inserting a value for a single key.
-///
-/// - parameters:
-///   - value: The value to substitute or insert for `key`.
-///   - key: The key whose value to substitute or insert.
-///
-/// - returns: A new dictionary that is identical to this except for the `value` of `key`.
 extension Dictionary {
+
+    /// Returns a copy of self dictionary substituting or inserting a value for a single key.
+    ///
+    /// - parameters:
+    ///   - value: The value to substitute or insert for `key`.
+    ///   - key: The key whose value to substitute or insert.
+    ///
+    /// - returns: A new dictionary that is identical to this except for the `value` of `key`.
     func updatingValue(_ value: Dictionary.Value?, forKey key: Dictionary.Key) -> [Key: Value] {
         var updated = self
         if let value = value {
