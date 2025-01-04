1cb06c29154b7a3a918ede27d89fed0a1ff5deb7
David Dávila <david@davi.la>
2025-01-04 14:51:43 -0600

    Create release, update versions

--- Release/toggltargets.rb
+++ Release/toggltargets.rb
@@ -1,7 +1,7 @@
 cask 'toggltargets' do
-    version '1.0.0'
-    sha256 'dfe06c33974a029f7cd669ec514e5792e28dd4503a6352e4d614f2a9df51a472'
-    url 'https://github.com/davila-vilanova/TogglTargets/releases/download/v1.0/TogglTargets.dmg'
+    version '1.1.0'
+    sha256 '4b8b69efb64946aba151747005abd40136e0108b54d82e7bc3ff4cbc174847d3'
+    url 'https://github.com/davila-vilanova/TogglTargets/releases/download/v1.1/TogglTargets.dmg'
     name 'TogglTargets'
     homepage 'https://github.com/davila-vilanova/TogglTargets'
     app 'TogglTargets.app'
